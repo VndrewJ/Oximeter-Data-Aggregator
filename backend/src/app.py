724d93cbc9f5518825a4b29fcfa7df0642f5a34e
Andrew <andyjkoh@gmail.com>
2025-08-23 16:32:35 +1200

    Edited flask app to return JSON API

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_cors import CORS
 from markupsafe import escape
 import csv
 import os
 
-app = Flask(__name__)  # <-- Add this line
+app = Flask(__name__) 
+CORS(app) 
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
     
 @app.route("/data")
-def goto_data():
+def get_data():
     data = []
-    # Go up one directory from src, then into data
     csv_path = os.path.join(os.path.dirname(__file__), "..", "data", "oximeter_test_data.csv")
-    csv_path = os.path.abspath(csv_path)  # Get absolute path
+    csv_path = os.path.abspath(csv_path)
     with open(csv_path, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             data.append(row)
-    return render_template("display_data.html", table_data=data)
+    return jsonify(data)
 
