b5a923bcf97dc201cae92563861ac8d1421b9a19
Andrew <andyjkoh@gmail.com>
2025-08-31 14:55:28 +1200

    cleaned up and reorganised repo updated requirements.txt

--- Vlad's Stuff/Python/scan_ble.py
+++
@@ -1,9 +0,0 @@
-from bleak import BleakScanner
-import asyncio
-
-async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(f"{d.name} - {d.address}")
-
-asyncio.run(main())
